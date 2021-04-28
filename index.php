8dd2583d76c996e0e79a7a861b6f0a7f94e5e42b
devdicova <83246898+devdicova@users.noreply.github.com>
2021-04-28 09:49:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
+<html><head><meta charset="utf-8"> </head>
+<body>
 <?php
-$con=mysqli_connect ("localhost", "root", "");
-mysqli_select_db($con, "fygp");
+$link = mysqli_connect("localhost", "root", "", "fygp");
+
+/* verificar la conexión */
+if (mysqli_connect_errno()) {
+    printf("Conexión fallida: %s\n", mysqli_connect_error());
+    exit();
+}
+
+if ($result = mysqli_query($link, "SELECT id, ids FROM app_proyecto ORDER BY id")) {
+
+    /* determinar el número de filas del resultado */
+    $row_cnt = mysqli_num_rows($result);
+
+    printf("El resultado tiene %d filas.\n", $row_cnt);
+
+    /* cerrar el resulset */
+    mysqli_free_result($result);
+}
+
+/* cerrar la conexión */
+mysqli_close($link);
 ?>
+</body></html>
