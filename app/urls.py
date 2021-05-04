c1f9b20a59e69fbb652c87808293b9c427e0285e
devdicova <dev.dicova@gmail.com>
2021-05-04 09:17:15 -0500

    new

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_view, name='app.home'),
-    url(r'^login/$', views.login_view, name='app.login'),
+    url(r'^admin/$', views.login_view, name='app.login'),
     url(r'^list/$', views.list_view, name='app.list'),
     url(r'^proyecto/(?P<slug>[^\.]+)/$', views.proyecto_view, name='app.proyecto'),
     url(r'^salir/$', views.logout_view, name='app.logout'),
