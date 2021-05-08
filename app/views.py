acfd0c550dd9b3d0ce63471824e2f53814845f83
devdicova <dev.dicova@gmail.com>
2021-05-08 09:43:43 -0500

    new

--- app/views.py
+++ app/views.py
@@ -60,7 +60,7 @@ def login_view(request):
 
 def list_view(request):
     userprofiles = UserProfile.objects.all().order_by('-create_at')
-    projects = Proyecto.objects.all().order_by('-create_at')
+    projects = Proyecto.objects.all().order_by('create_at')
     cssrules = Cssrules.objects.all().order_by('-create_at')
     template = 'app/admin/list.html'
     return render_to_response(template,locals(),context_instance=RequestContext(request))
@@ -489,7 +489,7 @@ def crearcssrule_view(request):
 
 def cssruleslist_view(request):
     userprofiles = UserProfile.objects.all().order_by('-create_at')
-    projects = Proyecto.objects.all().order_by('-create_at')
+    projects = Proyecto.objects.all().order_by('create_at')
     cssrules = Cssrules.objects.all().order_by('-create_at')
     template = 'app/admin/cssruleslist.html'
     return render_to_response(template,locals(),context_instance=RequestContext(request))
