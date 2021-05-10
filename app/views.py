cbbb6ac380f0a0a4fda0c6929cabfcb509d0eda1
devdicova <dev.dicova@gmail.com>
2021-05-10 12:00:58 -0500

    new

--- app/views.py
+++ app/views.py
@@ -33,7 +33,7 @@ from django.views.generic.list import ListView
 def home_view(request):
     message = ''
     userprofiles = UserProfile.objects.all().order_by('-create_at')
-    projects = Proyecto.objects.all().order_by('periodos')
+    projects = Proyecto.objects.all().order_by('ids')
     cssrules = Cssrules.objects.all().order_by('-create_at')
     template = 'app/home.html'
     return render_to_response(template,locals(),context_instance=RequestContext(request))
@@ -60,7 +60,7 @@ def login_view(request):
 
 def list_view(request):
     userprofiles = UserProfile.objects.all().order_by('-create_at')
-    projects = Proyecto.objects.all().order_by('periodos')
+    projects = Proyecto.objects.all().order_by('ids')
     cssrules = Cssrules.objects.all().order_by('-create_at')
     template = 'app/admin/list.html'
     return render_to_response(template,locals(),context_instance=RequestContext(request))
